eb335ec56bd2dafa6054810f1353710e6a45f0fd
wyy <wuyanyi09@gmail.com>
2013-05-18 22:01:51 -0700

    add vec_functs.tcc

--- cppcommon/vec_functs.h
+++ cppcommon/vec_functs.h
@@ -4,6 +4,22 @@
 #include <vector>
 #include <algorithm>
 #include <queue>
+#include <string>
+#include "vec_functs.tcc"
+
+#define PRINT_VECTOR(vec) for(size_t i = 0; i < vec.size(); i++) \
+{\
+	cout<<vec[i]<<endl;\
+}
+
+#define PRINT_MATRIX(mat) for(size_t i = 0; i < mat.size(); i++) \
+{\
+	for(size_t j = 0; j < mat[i].size(); j++)\
+	{\
+		cout<<mat[i][j]<<endl;\
+	}\
+}
+
 namespace CPPCOMMON
 {
 	using namespace std;
