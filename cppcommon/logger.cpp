8407cf269810a648fbc2bec86fce045a559466cd
wyy <wuyanyi09@gmail.com>
2013-05-25 02:17:58 +0800

    logger.h/cpp unfinished

--- cppcommon/logger.cpp
+++ cppcommon/logger.cpp
@@ -3,11 +3,33 @@ namespace CPPCOMMON
 {
 	Logger::Logger()
 	{
+		_isCoutOpen = true;
+		_logLevel[LOG_DEBUG] = "DEBUG";
+		_logLevel[LOG_INFO] = "INFO";
+		_logLevel[LOG_WARN] = "WARN";
+		_logLevel[LOG_ERROR] = "ERROR";
+		_logLevel[LOG_FATAL] = "FATAL";
 	}
 	Logger::~Logger()
 	{
 	}
 	//Logger::AddHandler
+	void Logger::InitDefault()
+	{
+	}
+	bool Logger::Logging(unsigned int level, const string& msg, const string& fileName, const int& lineNo)
+	{
+		if(level < LOG_DEBUG || level > LOG_FATAL)
+		{
+			cerr<<"level's value is out of range"<<endl;
+			return false;
+		}
+		string format = "%s:%d [%s]:%s";
+		sprintf(_cStrBuf, format.c_str(), fileName.c_str(), lineNo, _logLevel[level], msg.c_str());
+		if(_isCoutOpen)
+		  cout<<_cStrBuf<<endl;
+		return true;
+	}
 }
 
 
@@ -15,11 +37,15 @@ namespace CPPCOMMON
 using namespace CPPCOMMON;
 int main()
 {
-	fstream fout;
-	fout.open("tmp.log", ios::out|ios::app);
+	/*
+	ofstream fout;
+	fout.open("tmp.log", ios::app);
 	fout<<"11111"<<endl;
 	fout.close();
+	f2<<"222"<<endl;
+	*/
 	Logger logger;
+	logger.Logging(LOG_INFO, "test", __FILE__, __LINE__);
 	return 0;
 }
 #endif
--- cppcommon/logger.h
+++ cppcommon/logger.h
@@ -1,9 +1,19 @@
 #ifndef CPPCOMMON_LOGGER_H
 #define CPPCOMMON_LOGGER_H
 
+#define LOG_DEBUG 1
+#define LOG_INFO 2
+#define LOG_WARN 3
+#define LOG_ERROR 4
+#define LOG_FATAL 5
+#define LEVEL_ARRAY_SIZE 6
+#define CSTR_BUFFER_SIZE 1024
+
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <exception>
 namespace CPPCOMMON
 {
 	using namespace std;
@@ -13,9 +23,12 @@ namespace CPPCOMMON
 			Logger();
 			~Logger();
 		public:
+			void InitDefault();
+			bool Logging(unsigned int level, const string& msg, const string& fileName, const int& lineNo);
 		private:
-			//vector<ofstream>
-			
+			bool _isCoutOpen;
+			char _cStrBuf[CSTR_BUFFER_SIZE];
+			const char * _logLevel[LEVEL_ARRAY_SIZE];
 	};
 }
 
