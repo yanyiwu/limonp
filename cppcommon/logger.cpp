cc1feb4fd43149da4d490788768334326b8f1766
gwdwyy <wuyanyi09@gmail.com>
2013-05-25 12:14:19 +0800

    add singleton in cpp and macro function into .h

--- cppcommon/logger.cpp
+++ cppcommon/logger.cpp
@@ -1,35 +1,59 @@
 #include "logger.h"
 namespace CPPCOMMON
 {
+	const char * Logger::_logFormat =  "%s:%d [%s]:%s";
 	Logger::Logger()
 	{
 		_isCoutOpen = true;
-		_logLevel[LOG_DEBUG] = "DEBUG";
-		_logLevel[LOG_INFO] = "INFO";
-		_logLevel[LOG_WARN] = "WARN";
-		_logLevel[LOG_ERROR] = "ERROR";
-		_logLevel[LOG_FATAL] = "FATAL";
+		_logLevel[LL_DEBUG] = "DEBUG";
+		_logLevel[LL_INFO] = "INFO";
+		_logLevel[LL_WARN] = "WARN";
+		_logLevel[LL_ERROR] = "ERROR";
+		_logLevel[LL_FATAL] = "FATAL";
+		InitDefault();
 	}
 	Logger::~Logger()
 	{
+		if(_logFile)
+		{
+			_logFile.close();
+		}
 	}
 	//Logger::AddHandler
 	void Logger::InitDefault()
 	{
+		string logDir = "./log/";
+		string logName = "run.log";
+		if(!checkDirExist(logDir.c_str()))
+		{
+			createDir(logDir.c_str());
+		}
+		
+		_logFile.open((logDir + logName).c_str(), ios::app);
+		
 	}
 	bool Logger::Logging(unsigned int level, const string& msg, const string& fileName, const int& lineNo)
 	{
-		if(level < LOG_DEBUG || level > LOG_FATAL)
+		if(level < LL_DEBUG || level > LL_FATAL)
 		{
 			cerr<<"level's value is out of range"<<endl;
 			return false;
 		}
-		string format = "%s:%d [%s]:%s";
-		sprintf(_cStrBuf, format.c_str(), fileName.c_str(), lineNo, _logLevel[level], msg.c_str());
+		sprintf(_cStrBuf, _logFormat, fileName.c_str(), lineNo, _logLevel[level], msg.c_str());
 		if(_isCoutOpen)
+		{
 		  cout<<_cStrBuf<<endl;
+		}
+		if(_logFile)
+		{
+			_logFile<<_cStrBuf<<endl;
+		}
 		return true;
 	}
+}
+namespace CPPCOMMON
+{
+	Logger loggerSingleTon;
 }
 
 
@@ -44,8 +68,8 @@ int main()
 	fout.close();
 	f2<<"222"<<endl;
 	*/
-	Logger logger;
-	logger.Logging(LOG_INFO, "test", __FILE__, __LINE__);
+	//loggerSingleTon.Logging(LL_INFO, "test", __FILE__, __LINE__);
+	LogInfo("test info log");
 	return 0;
 }
 #endif
--- cppcommon/logger.h
+++ cppcommon/logger.h
@@ -1,22 +1,32 @@
 #ifndef CPPCOMMON_LOGGER_H
 #define CPPCOMMON_LOGGER_H
 
-#define LOG_DEBUG 1
-#define LOG_INFO 2
-#define LOG_WARN 3
-#define LOG_ERROR 4
-#define LOG_FATAL 5
-#define LEVEL_ARRAY_SIZE 6
-#define CSTR_BUFFER_SIZE 1024
-
 #include <vector>
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <exception>
+#include "file_functs.h"
+
+#define LL_DEBUG 1
+#define LL_INFO 2
+#define LL_WARN 3
+#define LL_ERROR 4
+#define LL_FATAL 5
+#define LEVEL_ARRAY_SIZE 6
+#define CSTR_BUFFER_SIZE 1024
+
+
+#define LogDebug(msg) loggerSingleTon.Logging(LL_DEBUG, msg, __FILE__, __LINE__)
+#define LogInfo(msg) loggerSingleTon.Logging(LL_INFO, msg, __FILE__, __LINE__)
+#define LogWarn(msg) loggerSingleTon.Logging(LL_WARN, msg, __FILE__, __LINE__)
+#define LogError(msg) loggerSingleTon.Logging(LL_ERROR, msg, __FILE__, __LINE__)
+#define LogFatal(msg) loggerSingleTon.Logging(LL_FATAL, msg, __FILE__, __LINE__)
+
+
+
 namespace CPPCOMMON
 {
-	using namespace std;
+	using namespace std
 	class Logger
 	{
 		public:
@@ -29,7 +39,14 @@ namespace CPPCOMMON
 			bool _isCoutOpen;
 			char _cStrBuf[CSTR_BUFFER_SIZE];
 			const char * _logLevel[LEVEL_ARRAY_SIZE];
+			ofstream _logFile;
+			static const char * _logFormat;
 	};
+}
+
+namespace CPPCOMMON
+{
+	extern Logger loggerSingleTon;
 }
 
 #endif
