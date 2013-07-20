1f7864a87daca836fe317c94c19ef28585ba5341
gwdwyy <wuyanyi09@gmail.com>
2013-07-20 13:17:04 +0800

    add header author into source files

---
+++ add_header.sh
@@ -0,0 +1,1 @@
+sed -i '1i/************************************\n * file enc : utf8\n * author   : wuyanyi09@gmail.com\n************************************/' ./cppcommon/*.h ./cppcommon/*.cpp ./cppcommon/*.tcc 
