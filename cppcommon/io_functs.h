54db5bbdb27e1a0af271fa380cfbadae5cf552fd
wyy <wuyanyi09@gmail.com>
2013-10-24 02:01:56 -0700

    mv io_functs.cpp into .h

--- cppcommon/io_functs.h
+++ cppcommon/io_functs.h
@@ -9,9 +9,46 @@
 namespace CPPCOMMON
 {
     using namespace std;
-    string loadFile2Str(const char * const filepath);
-    void loadStr2File(const char * const filename, ios_base::openmode mode, const string& str);
-    int ReadFromFile(const char * fileName, char* buf, int maxCount, const char* mode);
-    int WriteStr2File(const char* fileName, const char* buf, const char* mode);
+    inline string loadFile2Str(const char * const filepath)
+    {
+        ifstream in(filepath);
+        if(!in)
+        {
+            return "";
+        }
+        istreambuf_iterator<char> beg(in), end;
+        string str(beg, end);
+        in.close();
+        return str;
+    }
+    
+    inline void loadStr2File(const char * const filename, ios_base::openmode mode, const string& str)
+    {
+        ofstream out(filename, mode);
+        ostreambuf_iterator<char> itr (out);
+        copy(str.begin(), str.end(), itr);
+        out.close();
+    }
+
+    inline int ReadFromFile(const char * fileName, char* buf, int maxCount, const char* mode)
+    {                                        
+        FILE* fp = fopen(fileName, mode);    
+        if (!fp)                             
+          return 0;                          
+        int ret;                             
+        fgets(buf, maxCount, fp) ? ret = 1 : ret = 0;                                       
+        fclose(fp);                          
+        return ret;                          
+    }                                        
+
+    inline int WriteStr2File(const char* fileName, const char* buf, const char* mode)
+    {                                          
+        FILE* fp = fopen(fileName, mode);      
+        if (!fp)                               
+          return 0;                            
+        int n = fprintf(fp, "%s", buf);        
+        fclose(fp);                            
+        return n;                              
+    }                                          
 }
 #endif
