ce231b92fca53003ec23d7de55ab609b8949d538
aholic <ruochen.xu@gmail.com>
2013-12-17 05:24:16 +0800

    add unitest for md5 and testdata for it

--- test/unittest/TMd5.cpp
+++ test/unittest/TMd5.cpp
@@ -16,6 +16,14 @@ const char* const DICT_FILE_MD5[] = {
     "55f1116c05c8051ab53171f0b7455197",
     "b123553a2418c4bda51abc64d705d5d4"};
 
+const char* const TEST_STR[] = {
+    "0000"
+};
+
+const char* const TEST_STR_MD5[] = {
+    "4a7d1ed414474e4033ac29ccb8653d9b"
+};
+
 TEST(Md5Test, Test1)
 {
     ASSERT_EQ(sizeof(DICT_FILE)/sizeof(DICT_FILE[0]), sizeof(DICT_FILE_MD5)/sizeof(DICT_FILE_MD5[0]));
@@ -24,6 +32,13 @@ TEST(Md5Test, Test1)
     {
         md5File(DICT_FILE[i], tmp);
         ASSERT_EQ(tmp, string(DICT_FILE_MD5[i]));
+    }
+
+    ASSERT_EQ(sizeof(TEST_STR)/sizeof(TEST_STR[0]), sizeof(TEST_STR_MD5)/sizeof(TEST_STR_MD5[0]));
+    for (uint i = 0; i < sizeof(TEST_STR)/sizeof(TEST_STR[0]); i++)
+    {
+        md5String(TEST_STR[i], tmp);
+        ASSERT_EQ(tmp, string(TEST_STR_MD5[i]));
     }
 }
 
