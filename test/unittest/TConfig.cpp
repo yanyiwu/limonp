202442c709d8dc53394214cfc1cc665ea6f11caa
wyy <wuyanyi09@gmail.com>
2013-12-23 23:25:36 -0800

    modify Tconfig.cpp

--- test/unittest/TConfig.cpp
+++ test/unittest/TConfig.cpp
@@ -6,9 +6,10 @@ TEST(COnfigTest, Test1)
 {
     Config gConfig("../test/testdata/1.conf");
     string res;
-    res << gConfig;
-    print(res);
+    ASSERT_EQ("{key1:val1, key2:val2}", res << gConfig);
     gConfig.get("key1", res);
+    ASSERT_EQ("val1", res);
     gConfig.get("key2", res);
+    ASSERT_EQ("val2", res);
 }
 
