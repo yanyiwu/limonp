dc6810e08f494de9817193b235594fbd44621af1
wyy <wuyanyi09@gmail.com>
2014-03-17 21:14:27 +0800

    modify logger && rm map_functs.hpp

--- test/unittest/TLogger.cpp
+++ test/unittest/TLogger.cpp
@@ -7,20 +7,20 @@ using namespace Limonp;
 #define LOGGER_LEVEL LL_INFO
 TEST(LoggerTest, Test1)
 {
-    ASSERT_TRUE(LogInfo("hehe"));
-    ASSERT_TRUE(LogDebug("hehe"));
-    ASSERT_TRUE(LogWarn("hehe"));
-    ASSERT_TRUE(LogError("hehe"));
-    ASSERT_TRUE(LogFatal("hehe"));
-    ASSERT_TRUE(LogInfo("str[%s] int[%d]", "str1",15));
+    LogInfo("hehe");
+    LogDebug("hehe");
+    LogWarn("hehe");
+    LogError("hehe");
+    LogFatal("hehe");
+    LogInfo("str[%s] int[%d]", "str1",15);
 }
 
 TEST(LoggerTest, Test2)
 {
-    ASSERT_TRUE(LogInfo("hehe"));
-    ASSERT_TRUE(LogDebug("hehe"));
-    ASSERT_TRUE(LogWarn("hehe"));
-    ASSERT_TRUE(LogError("hehe"));
-    ASSERT_TRUE(LogFatal("hehe"));
-    ASSERT_TRUE(LogInfo("str[%s] int[%d]", "str1",15));
+    LogInfo("hehe");
+    LogDebug("hehe");
+    LogWarn("hehe");
+    LogError("hehe");
+    LogFatal("hehe");
+    LogInfo("str[%s] int[%d]", "str1",15);
 }
