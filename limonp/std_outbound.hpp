89c0798b7f555806c3cc588a99bdc3604bcf3043
wyy <wuyanyi09@gmail.com>
2014-03-27 11:39:54 +0800

    remove -std=c++0x dependency to adapter lower version g++

--- limonp/std_outbound.hpp
+++ limonp/std_outbound.hpp
@@ -5,11 +5,14 @@
 
 #if(__cplusplus == 201103L)
 #include <unordered_map>
+#include <unordered_set>
 #else
 #include <tr1/unordered_map>
+#include <tr1/unordered_set>
 namespace std
 {
     using std::tr1::unordered_map;
+    using std::tr1::unordered_set;
 }
 
 #endif
