59870a0211ec382c16d508b4343ea7952e827535
wyy <wuyanyi09@gmail.com>
2014-05-02 17:12:49 +0800

    finished tmutexlock test

--- test/unittest/TMutexLock.cpp
+++ test/unittest/TMutexLock.cpp
@@ -1,7 +1,13 @@
 #include "gtest/gtest.h"
+#include "std_outbound.hpp"
 #include "MutexLock.hpp"
 
 using namespace Limonp;
+
+const size_t THREAD_SUM = 3;
+const size_t FOR_SIZE = 3;
+
+vector<size_t> res;
 
 struct PthreadInfo
 {
@@ -10,35 +16,46 @@ struct PthreadInfo
     MutexLock* ptMutexLock;
 };
 
-void* worker(void * arg)
+void* workerNoLocked(void * arg)
 {
-    PthreadInfo * ptInfo = (PthreadInfo *) arg;
-    //MutexLockGuard lock(*ptInfo->ptMutexLock);
-    for(size_t i = 0; i < 3; i ++)
+    for(size_t i = 0; i < FOR_SIZE; i ++)
     {
-        cout << ptInfo->id << ':' << i << endl;
-        usleep(100*i);
+        //cout << ptInfo->id << ':' << i << endl;
+        res.push_back(i);
+        usleep(100 * i);
     }
-
     return NULL;
 }
 
-class TMutexLock
+void* workerLocked(void * arg)
+{
+    PthreadInfo * ptInfo = (PthreadInfo *) arg;
+    MutexLockGuard lock(*ptInfo->ptMutexLock);
+    for(size_t i = 0; i < FOR_SIZE; i ++)
+    {
+        //cout << ptInfo->id << ':' << i << endl;
+        res.push_back(i);
+        usleep(100 * i);
+    }
+    return NULL;
+}
+
+class ThreadsNoLocked
 {
     private:
         MutexLock _mutex;
         vector<PthreadInfo> _pthreadInfos;
     public:
-        TMutexLock(size_t threadSum): _pthreadInfos(threadSum)
+        ThreadsNoLocked(size_t threadSum): _pthreadInfos(threadSum)
         {
             for(size_t i = 0; i < _pthreadInfos.size(); i++)
             {
                 _pthreadInfos[i].id = i;
                 _pthreadInfos[i].ptMutexLock = &_mutex;
-                CHECK(pthread_create(&_pthreadInfos[i].pthread_id, NULL, worker, &_pthreadInfos[i]));
+                CHECK(pthread_create(&_pthreadInfos[i].pthread_id, NULL, workerNoLocked, &_pthreadInfos[i]));
             }
         }
-        ~TMutexLock(){}
+        ~ThreadsNoLocked(){}
     public:
         void start()
         {
@@ -49,12 +66,43 @@ class TMutexLock
         }
 };
 
+class ThreadsLocked
+{
+    private:
+        MutexLock _mutex;
+        vector<PthreadInfo> _pthreadInfos;
+    public:
+        ThreadsLocked(size_t threadSum): _pthreadInfos(threadSum)
+        {
+            for(size_t i = 0; i < _pthreadInfos.size(); i++)
+            {
+                _pthreadInfos[i].id = i;
+                _pthreadInfos[i].ptMutexLock = &_mutex;
+                CHECK(pthread_create(&_pthreadInfos[i].pthread_id, NULL, workerLocked, &_pthreadInfos[i]));
+            }
+        }
+        ~ThreadsLocked(){}
+    public:
+        void start()
+        {
+            for(size_t i = 0; i < _pthreadInfos.size(); i++)
+            {
+                CHECK(pthread_join(_pthreadInfos[i].pthread_id, NULL));
+            }
+        }
+};
 
 TEST(MutexLock, Test1)
 {
-    size_t threadSum = 3;
-    TMutexLock tMutex(threadSum);
-    tMutex.start();
+    string str;
+    ThreadsNoLocked noLock(THREAD_SUM);
+    res.clear();
+    noLock.start();
+    ASSERT_EQ(str << res, "[\"0\", \"0\", \"0\", \"1\", \"1\", \"1\", \"2\", \"2\", \"2\"]");
+    ThreadsLocked locked(THREAD_SUM);
+    res.clear();
+    locked.start();
+    ASSERT_EQ(str << res,  "[\"0\", \"1\", \"2\", \"0\", \"1\", \"2\", \"0\", \"1\", \"2\"]");
 }
 
 
