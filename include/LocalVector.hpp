0fba41750711871a9d4d8d21b25898939d557732
wyy <wuyanyi09@gmail.com>
2014-06-05 01:01:36 +0800

    updatte localvector, and test ok

--- include/LocalVector.hpp
+++ include/LocalVector.hpp
@@ -9,16 +9,45 @@
 namespace Limonp
 {
     using namespace std;
-    const size_t LOCAL_VECTOR_BUFFER_SIZE = 32;
+    const size_t LOCAL_VECTOR_BUFFER_SIZE = 16;
     template <class T>
         class LocalVector
         {
             public:
-                typedef T* const_iterator ;
+                typedef const T* const_iterator ;
+                typedef T value_type;
+                typedef size_t size_type;
+            private:
+                T _buffer[LOCAL_VECTOR_BUFFER_SIZE];
+                T * _ptr;
+                size_t _size;
+                size_t _capacity;
             public:
-                LocalVector(): _ptr(_buffer), _size(0), _capacity(LOCAL_VECTOR_BUFFER_SIZE)
+                LocalVector()
                 {
+                    _init();
                 };
+                LocalVector(const LocalVector<T>& vec)
+                {
+                    _init();
+                    *this = vec;
+                }
+                LocalVector(const_iterator  begin, const_iterator end) // TODO: make it faster
+                {
+                    _init();
+                    while(begin != end)
+                    {
+                        push_back(*begin++);
+                    }
+                }
+                LocalVector(size_t size, const T& t) // TODO: make it faster
+                {
+                    _init();
+                    while(size--)
+                    {
+                        push_back(t);
+                    }
+                }
                 ~LocalVector()
                 {
                     if(_ptr != _buffer)
@@ -27,7 +56,37 @@ namespace Limonp
                     }
                 };
             public:
+                LocalVector<T>& operator = (const LocalVector<T>& vec)
+                {
+                    clear();
+                    _size = vec.size();
+                    _capacity = vec.capacity();
+                    if(vec._buffer == vec._ptr)
+                    {
+                        memcpy(_buffer, vec._buffer, sizeof(T) * _size);
+                        _ptr = _buffer;
+                    }
+                    else
+                    {
+                        _ptr = (T*) malloc(vec.capacity() * sizeof(T));
+                        assert(_ptr);
+                        memcpy(_ptr, vec._ptr, vec.size() * sizeof(T));
+                    }
+                    return *this;
+                }
+            private:
+                void _init()
+                {
+                    _ptr = _buffer;
+                    _size = 0;
+                    _capacity = LOCAL_VECTOR_BUFFER_SIZE;
+                }
+            public:
                 T& operator [] (size_t i) 
+                {
+                    return _ptr[i];
+                }
+                const T& operator [] (size_t i) const
                 {
                     return _ptr[i];
                 }
@@ -38,6 +97,7 @@ namespace Limonp
                         _ptr[_size++] = t;
                         return ;
                     }
+                    assert(_capacity);
                     reserve(_capacity * 2);
                     _ptr[_size ++ ] = t;
                 }
@@ -82,12 +142,32 @@ namespace Limonp
                 {
                     return _ptr + _size;
                 }
-            private:
-                T _buffer[LOCAL_VECTOR_BUFFER_SIZE];
-                T * _ptr;
-                size_t _size;
-                size_t _capacity;
+                void clear()
+                {
+                    if(_ptr != _buffer)
+                    {
+                        free(_ptr);
+                    }
+                    _init();
+                }
         };
+
+    template <class T>
+        ostream & operator << (ostream& os, const LocalVector<T>& vec)
+        {
+            if(vec.empty())
+            {
+                return os << "[]";
+            }
+            os<<"[\""<<vec[0];
+            for(size_t i = 1; i < vec.size(); i++)
+            {
+                os<<"\", \""<<vec[i];
+            }
+            os<<"\"]";
+            return os;
+        }
+
 }
 
 #endif
--- test/unittest/TLocalVector.cpp
+++ test/unittest/TLocalVector.cpp
@@ -20,4 +20,28 @@ TEST(LocalVector, test1)
     ASSERT_EQ(vec.size(), size);
     ASSERT_EQ(vec.capacity(), 256);
     ASSERT_FALSE(vec.empty());
+    LocalVector<size_t> vec2(vec);
+    ASSERT_EQ(vec2.capacity(), vec.capacity());
+    ASSERT_EQ(vec2.size(), vec.size());
+}
+
+TEST(LocalVector, test2)
+{
+    LocalVector<size_t> vec;
+    ASSERT_EQ(vec.size(), 0);
+    ASSERT_EQ(vec.capacity(), LOCAL_VECTOR_BUFFER_SIZE);
+    ASSERT_TRUE(vec.empty());
+    ASSERT_FALSE(vec.full());
+    size_t size = 1;
+    for(size_t i = 0; i < size; i++)
+    {
+        vec.push_back(i);
+    }
+    ASSERT_EQ(vec.size(), size);
+    ASSERT_EQ(vec.capacity(), LOCAL_VECTOR_BUFFER_SIZE);
+    ASSERT_FALSE(vec.empty());
+    LocalVector<size_t> vec2;
+    vec2 = vec;
+    ASSERT_EQ(vec2.capacity(), vec.capacity());
+    ASSERT_EQ(vec2.size(), vec.size());
 }
