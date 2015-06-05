69763b67fd4147d1259680342dee4fd051cd1f0d
yanyiwu <wuyanyi09@foxmail.com>
2015-06-05 19:31:59 +0800

    update function config::get

--- test/unittest/TConfig.cpp
+++ test/unittest/TConfig.cpp
@@ -6,9 +6,9 @@ TEST(COnfigTest, Test1) {
   Config conf("../test/testdata/1.conf");
   string res;
   ASSERT_EQ("{key1:val1, key2:val2}", res << conf);
-  conf.get("key1", res);
+  res = conf.get("key1", "");
   ASSERT_EQ("val1", res);
-  conf.get("key2", res);
+  res = conf.get("key2", "");
   ASSERT_EQ("val2", res);
   const char * str = conf["key3"];
   ASSERT_FALSE(str);
